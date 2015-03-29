d284bfa23f5d79d82e3e53471f1a169eaa4ab996
GhostFlying <GhostFlying@Gmail.com>
2015-03-29 22:30:07 +0800

    set delayed alarm if user is active

--- app/src/main/java/com/ghostflying/autobatterysaver/service/WorkService.java
+++ app/src/main/java/com/ghostflying/autobatterysaver/service/WorkService.java
@@ -1,8 +1,10 @@
 package com.ghostflying.autobatterysaver.service;
 
+import android.app.AlarmManager;
 import android.app.IntentService;
-import android.content.Intent;
+import android.app.PendingIntent;
 import android.content.Context;
+import android.content.Intent;
 import android.os.PowerManager;
 import android.provider.Settings;
 
@@ -18,6 +20,8 @@ public class WorkService extends IntentService {
     private static final String ACTION_START = BuildConfig.APPLICATION_ID + ".START_SLEEP";
     private static final String ACTION_STOP = BuildConfig.APPLICATION_ID + ".STOP_SLEEP";
     private static final long USER_INACTIVITY_THRESHOLD = 15L * 60L * 1000L;
+    private static final long DELAYED_TIME_MILLISECONDS = 15L * 60L * 1000L;
+    private static final int START_INTENT_REQUEST_CODE = 1;
 
     public WorkService() {
         super("WorkService");
@@ -26,43 +30,54 @@ public class WorkService extends IntentService {
     @Override
     protected void onHandleIntent(Intent intent) {
         if (intent != null) {
-            if (ACTION_START.equals(intent.getAction())){
+            if (ACTION_START.equals(intent.getAction())) {
                 enableSleepModeIfNeeded();
             }
         }
     }
 
     private void enableSleepModeIfNeeded() {
-        PowerManager manager = (PowerManager)getSystemService(Context.POWER_SERVICE);
-        if (manager.isInteractive()){
-            // screen is on, the user use the device now.
-            //TODO set next alarm
+        PowerManager manager = (PowerManager) getSystemService(Context.POWER_SERVICE);
+        Date lastScreenOffTime = UserDetectorService.lastScreenOffTime;
+        if (manager.isInteractive()
+                || lastScreenOffTime == null // this should not happen
+                || (new Date().getTime() - lastScreenOffTime.getTime()) < USER_INACTIVITY_THRESHOLD) {
+            // screen is on or the user use the device before little time..
+            setDelayedAlarm();
             return;
         }
 
-        Date lastScreenOffTime = UserDetectorService.lastScreenOffTime;
-        if (lastScreenOffTime != null &&
-                (new Date().getTime() - lastScreenOffTime.getTime()) > USER_INACTIVITY_THRESHOLD){
-            WorkingMode mode = SettingUtil.getWorkingMode(this);
-            switch (mode){
-                case BATTERY_SAVER:
-                    setBatterySaverMode(true);
-                    break;
-                case AIRPLANE:
-                    setAirplaneMode(true);
-                    break;
-                case BOTH:
-                    setBatterySaverMode(true);
-                    setAirplaneMode(true);
-            }
-            stopService(new Intent(this, UserDetectorService.class));
+        WorkingMode mode = SettingUtil.getWorkingMode(this);
+        switch (mode) {
+            case BATTERY_SAVER:
+                setBatterySaverMode(true);
+                break;
+            case AIRPLANE:
+                setAirplaneMode(true);
+                break;
+            case BOTH:
+                setBatterySaverMode(true);
+                setAirplaneMode(true);
         }
+        stopService(new Intent(this, UserDetectorService.class));
     }
 
-    private void setBatterySaverMode(boolean mode){
-        PowerManager manager = (PowerManager)getSystemService(Context.POWER_SERVICE);
+    private void setDelayedAlarm() {
+        AlarmManager alarmManager = (AlarmManager) getSystemService(Context.ALARM_SERVICE);
+        long runTime = new Date().getTime() + DELAYED_TIME_MILLISECONDS;
+        Intent workIntent = new Intent(this, WorkService.class);
+        workIntent.setAction(ACTION_START);
+        alarmManager.set(
+                AlarmManager.RTC_WAKEUP,
+                runTime,
+                PendingIntent.getService(this, START_INTENT_REQUEST_CODE, workIntent, 0)
+        );
+    }
+
+    private void setBatterySaverMode(boolean mode) {
+        PowerManager manager = (PowerManager) getSystemService(Context.POWER_SERVICE);
         boolean batterySaverModeNow = manager.isPowerSaveMode();
-        if (batterySaverModeNow != mode){
+        if (batterySaverModeNow != mode) {
             if (mode)
                 BatterySaverModeUtil.enable();
             else
@@ -70,8 +85,8 @@ public class WorkService extends IntentService {
         }
     }
 
-    private void setAirplaneMode(boolean mode){
-        if (isAirplaneMode() != mode){
+    private void setAirplaneMode(boolean mode) {
+        if (isAirplaneMode() != mode) {
             if (mode)
                 AirplaneModeUtil.enable();
             else
@@ -79,7 +94,7 @@ public class WorkService extends IntentService {
         }
     }
 
-    private boolean isAirplaneMode(){
+    private boolean isAirplaneMode() {
         return Settings.Global.getInt(getContentResolver(), Settings.Global.AIRPLANE_MODE_ON, 0) != 0;
     }
 }
